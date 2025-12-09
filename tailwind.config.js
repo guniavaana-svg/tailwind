aa64f483bec3fb58d117097e965e3ed013a27774
guniavaana-svg <guniavaana@itvet.ge>
2025-12-09 07:30:03 +0000

    change1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      col1:'#666666',
+      col2: '#000000',
+    },
+  },
+  plugins: [],
+}
